20dad211fa0a8c43f4ee03b6f57af6abff2bab4b
Henry Abel <henryabel@fastmail.com>
2017-01-29 18:55:12 -0500

    Assignment 2: completed

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,81 +1,71 @@
-module.exports = function(grunt) {
-
-    grunt.registerTask( 'default', [ 'clean', 'copy', 'hapi', 'watch'] );
-
-    grunt.registerTask( 'build', [ 'clean', 'copy' ] );
-
-    grunt.registerTask( 'run', [ 'hapi', 'watch' ]);
-
+module.exports = function (grunt) {
+    grunt.registerTask('default', ['clean', 'copy', 'hapi', 'watch']);
+    grunt.registerTask('build', ['clean', 'copy']);
+    grunt.registerTask('run', ['hapi', 'watch']);
     grunt.initConfig({
-
         watch: {
             hapi: {
                 files: [
-                    './app/assets/**/*.{png,jpg,jpeg,mp3}',
-                    './app/scripts/**/*.js',
-                    './app/styles/**/*.css',
-                    './app/pages/**/*.html',
-                    './app/templates/**/*.html',
-                    'Gruntfile.js'
-                ],
-                tasks: [
-                    'clean',
-                    'copy'
-                ],
-                options: {
+                    './app/assets/**/*.{png,jpg,jpeg,mp3}'
+                    , './app/scripts/**/*.js'
+                    , './app/styles/**/*.css'
+                    , './app/pages/**/*.html'
+                    , './app/templates/**/*.html'
+                    , 'Gruntfile.js'
+                ]
+                , tasks: [
+                    'clean'
+                    , 'copy'
+                ]
+                , options: {
                     spawn: false
                 }
             }
-        },
-
-        copy: {
+        }
+        , copy: {
             dist: {
                 files: [{
-                    expand: true,
-                    src: [ './assets/**/*.{png,jpg,jpeg,mp3}' ],
-                    dest: './dist',
-                    cwd: './app'
+                    expand: true
+                    , src: ['./assets/**/*.{png,jpg,jpeg,mp3}']
+                    , dest: './dist'
+                    , cwd: './app'
                 }, {
-                    expand: true,
-                    src: [ './**/*.html' ],
-                    dest: './dist',
-                    cwd: './app/pages'
+                    expand: true
+                    , src: ['./**/*.html']
+                    , dest: './dist'
+                    , cwd: './app/pages'
                 }, {
-                    expand: true,
-                    src: [ './**/*.css' ],
-                    dest: './dist/styles',
-                    cwd: './app/styles'
+                    expand: true
+                    , src: ['./**/*.css']
+                    , dest: './dist/styles'
+                    , cwd: './app/styles'
                 }, {
-                    expand: true,
-                    src: [ './**/*.js' ],
-                    dest: './dist/scripts',
-                    cwd: './app/scripts'
+                    expand: true
+                    , src: ['./**/*.js']
+                    , dest: './dist/scripts'
+                    , cwd: './app/scripts'
                 }, {
-                    expand: true,
-                    src: [ './**/*.html' ],
-                    dest: './dist/templates',
-                    cwd: './app/templates'
+                    expand: true
+                    , src: ['./**/*.html']
+                    , dest: './dist/templates'
+                    , cwd: './app/templates'
                 }]
             }
-        },
-
-        hapi: {
+        }
+        , hapi: {
             custom_options: {
                 options: {
-                    server: require('path').resolve('./server'),
-                    bases: {
+                    server: require('path').resolve('./server')
+                    , bases: {
                         '/dist': require('path').resolve('./dist/')
                     }
                 }
             }
-        },
-
-        clean: ['./dist']
+        }
+        , clean: ['./dist']
     });
-
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-hapi');
-
 };
