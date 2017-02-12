d29efb39321c7b3efc846ab3ac83b20c3b448d83
Henry Abel <henryabel@fastmail.com>
2017-02-12 17:57:07 -0500

    comitting dist

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -6,12 +6,14 @@
     });
     $stateProvider.state('landing', {
         url: '/'
+        , controller: 'LandingCtrl as landing'
         , templateUrl: '/templates/landing.html'
     }).state('album', {
         url: '/album'
         , templateUrl: '/templates/album.html'
     }).state('collection', {
         url: '/collection'
+        , controller: 'CollectionCtrl as collection'
         , templateUrl: '/templates/collection.html'
     });
     angular.module('blocJams', ['ui.router']).config(config);
