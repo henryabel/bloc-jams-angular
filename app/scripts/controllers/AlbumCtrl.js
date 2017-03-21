3c09d19fb98a49e8b373dc5c4c0b965f28006930
Henry Abel <henryabel@fastmail.com>
2017-03-20 20:21:57 -0400

    Assignment 5: completed

---
+++ app/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,7 @@
+(function () {
+    function AlbumCtrl() {
+        this.albumData = albumPicasso;
+        console.log(this.albumData);
+    }
+    angular.module('blocJams').controller('AlbumCtrl', AlbumCtrl);
+})();
