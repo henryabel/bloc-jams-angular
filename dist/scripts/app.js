5a94f9ffceceda826ef7e3676317fd4dc8c26286
Henry Abel <henryabel@fastmail.com>
2017-03-20 20:27:47 -0400

    int commit

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,8 +1,4 @@
 (function () {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> a5
     function config($stateProvider, $locationProvider) {
         $locationProvider.html5Mode({
             enabled: true
@@ -25,27 +21,5 @@
             , templateUrl: '/templates/collection.html'
         });
     }
-<<<<<<< HEAD
-=======
-    function config($stateProvider, $locationProvider) {}
-    $locationProvider.html5Mode({
-        enabled: true
-        , requireBase: false
-    });
-    $stateProvider.state('landing', {
-        url: '/'
-        , controller: 'LandingCtrl as landing'
-        , templateUrl: '/templates/landing.html'
-    }).state('album', {
-        url: '/album'
-        , templateUrl: '/templates/album.html'
-    }).state('collection', {
-        url: '/collection'
-        , controller: 'CollectionCtrl as collection'
-        , templateUrl: '/templates/collection.html'
-    });
->>>>>>> c5
-=======
->>>>>>> a5
     angular.module('blocJams', ['ui.router']).config(config);
 })();
